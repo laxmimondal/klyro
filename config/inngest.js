a53a90a2e1f009214d20fa6984a0a9467d08d7a4
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2025-07-11 19:59:35 +0530

    Update inngest.js

--- config/inngest.js
+++ config/inngest.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import { connect } from "mongoose";
 import connectDB from "./db";
-import User from "@models/user";
+import User from "../models/user";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "klyro-next" });
